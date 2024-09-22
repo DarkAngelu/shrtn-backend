45612e449dc02161af869d76d813ce364e4027df
DarkAngelu <iamdarkangelu@gmail.com>
2024-09-22 20:06:27 +0530

    Forgot to declare axios this time lol

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const { PrismaClient } = require("@prisma/client")
 const express = require("express")
 const cors = require("cors")
+const axios = require("axios")
 
 const app = express()
 const port = process.env.PORT
