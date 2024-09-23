d8ebff300ef3160ec777fdab5e28f30d750526e1
DarkAngelu <iamdarkangelu@gmail.com>
2024-09-23 14:01:46 +0530

    Made a new service in aivencloud

--- server.js
+++ server.js
@@ -104,12 +104,12 @@ const url = `https://shrtn-etj2.onrender.com/`; // Replace with your Render URL
 const interval = 30000; // Interval in milliseconds (30 seconds)
 
 function reloadWebsite() {
-  axios.get(url)
+    axios.get(url)
     .then(response => {
-      console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
+        console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
     })
     .catch(error => {
-      console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
+        console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
     });
 }
 
